9b9fe1e5c0c96add2dc1d7662a604e338083d081
Darshit Dave <darshitdave02@gmail.com>
2023-01-18 12:49:24 +0530

    fix: add eslint and gitignore file

--- scoreCalculation.js
+++ scoreCalculation.js
@@ -1,78 +1,79 @@
 const calculateScore = (scores) => {
 
 
-    if (!Array.isArray(scores)) {
-        throw new Error('Input is not an array');
+  if (!Array.isArray(scores)) {
+    throw new Error('Input is not an array');
+  }
+
+  if (!scores.every(element => typeof element === 'number')) {
+    throw new Error('Array should consist of numbers only');
+  }
+
+  if (!(scores.length === 20 || scores.length === 21)) {
+    throw new Error('Invalid Input');
+  }
+
+  if (scores.length === 21 && (scores[18] + scores[19] < 10)) {
+    throw new Error('Invalid Input');
+  }
+
+
+
+  let finalScore = 0;
+
+  for (let i = 0; i < scores.length;) {
+    if (i < 18) {
+      if (scores[i] === 10) {
+        finalScore += scores[i] + scores[i + 1] + scores[i + 2];
+        i++;
+      }
+
+      else if (scores[i] + scores[i + 1] === 10) {
+        finalScore = scores[i] + scores[i + 1] + scores[i + 2];
+        i += 2;
+      }
+
+      else {
+        finalScore += scores[i] + scores[i + 1];
+        i += 2;
+      }
     }
-
-    if (!scores.every(element => typeof element === 'number')) {
-        throw new Error('Array should consist of numbers only');
+    else {
+      finalScore += scores[i];
+      i++;
     }
+  }
 
-    if (!(scores.length === 20 || scores.length === 21)) {
-        throw new Error('Invalid Input');
-    }
-
-    if (scores.length === 21 && (scores[18] + scores[19] < 10)) {
-        throw new Error('Invalid Input');
-    }
-
-
-
-    let finalScore = 0;
-
-    for (let i = 0; i < scores.length;) {
-        if (i < 18) {
-            if (scores[i] === 10) {
-                finalScore += scores[i] + scores[i + 1] + scores[i + 2];
-                i++;
-            }
-
-            else if (scores[i] + scores[i + 1] === 10) {
-                finalScore = scores[i] + scores[i + 1] + scores[i + 2];
-                i += 2;
-            }
-
-            else {
-                finalScore += scores[i] + scores[i + 1];
-                i += 2;
-            }
-        }
-        else {
-            finalScore += scores[i];
-            i++;
-        }
-    }
-
-    return finalScore;
+  return finalScore;
 
 
 
 
-}
+};
 
 
 let games = [[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10],
-[6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10],
+  [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
 const bestScore = (games) => {
 
-    let array = [];
-    array = games.map((game) => {
-        let result = calculateScore(game);
-        return result;
-    })
-    console.log(array);
+  let array = [];
+  array = games.map((game) => {
+    let result = calculateScore(game);
+    return result;
+  });
+  console.log(array);
 
-    return Math.max(...array);
+  return Math.max(...array);
 
-}
+};
 
-module.exports = { bestScore, calculateScore }
+module.exports = { bestScore, calculateScore };
 
 console.log(bestScore(games));
 
 // console.log(calculateScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]))
 // console.log(calculateScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]))
 // console.log(calculateScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
+
--- scoreCalculation.test.js
+++ scoreCalculation.test.js
@@ -1,29 +1,29 @@
-const { bestScore, calculateScore} = require('./scoreCalculation')
+const {  calculateScore} = require('./scoreCalculation');
 
 describe('Score Calculation',() => {
-    describe('Checking Inputs',() => {
+  describe('Checking Inputs',() => {
   
-      it('Should throw an error when input is not an array',() => {
+    it('Should throw an error when input is not an array',() => {
               
              
-        expect(()=> calculateScore('abs')).toThrow('Input is not an array');
-      });
+      expect(()=> calculateScore('abs')).toThrow('Input is not an array');
+    });
   
-      it('Should throw an error when array has an element which is not a number',() => {
+    it('Should throw an error when array has an element which is not a number',() => {
               
              
-        expect(()=> calculateScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 'hello', 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toThrow('Array should consist of numbers only');
-      });
+      expect(()=> calculateScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 'hello', 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toThrow('Array should consist of numbers only');
+    });
 
-      it('Should throw an error when frame size is not valid',() => {
+    it('Should throw an error when frame size is not valid',() => {
               
              
-        expect(()=> calculateScore([3, 6, 3, 6, 6, 3, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toThrow('Invalid Input');
-      });
+      expect(()=> calculateScore([3, 6, 3, 6, 6, 3, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toThrow('Invalid Input');
+    });
 
 
       
 
-    })
+  });
   
-    });
+});
