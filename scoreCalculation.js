ce63018e401b16204be8444e0053aff43d336233
Darshit Dave <darshitdave02@gmail.com>
2023-01-18 12:19:43 +0530

    feat: add functionality to take rolls for 1 game and give total score

---
+++ scoreCalculation.js
@@ -0,0 +1,56 @@
+const calculateScore = (scores) => {
+
+
+    if (!Array.isArray(scores)) {
+        throw new Error('Input is not an array');
+    }
+
+    if (!scores.every(element => typeof element === 'number')) {
+        throw new Error('Array should consist of numbers only');
+    }
+
+    if (!(scores.length === 20 || scores.length === 21)) {
+        throw new Error('Invalid Input');
+    }
+
+    if (scores.length === 21 && (scores[18] + scores[19] < 10)) {
+        throw new Error('Invalid Input');
+    }
+
+
+
+    let finalScore = 0;
+
+    for (let i = 0; i < scores.length;) {
+        if (i < 18) {
+            if (scores[i] === 10) {
+                finalScore += scores[i] + scores[i + 1] + scores[i + 2];
+                i++;
+            }
+
+            else if (scores[i] + scores[i + 1] === 10) {
+                finalScore = scores[i] + scores[i + 1] + scores[i + 2];
+                i += 2;
+            }
+
+            else {
+                finalScore += scores[i] + scores[i + 1];
+                i += 2;
+            }
+        }
+        else {
+            finalScore += scores[i];
+            i++;
+        }
+    }
+
+    return finalScore;
+
+
+
+
+}
+
+console.log(calculateScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]))
+console.log(calculateScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]))
+console.log(calculateScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
