880fb48d1e4cd2f587939e824027ba7222f640a5
Darshit Dave <darshitdave02@gmail.com>
2023-01-18 12:38:27 +0530

    chore: add test file with input validation tests

---
+++ scoreCalculation.test.js
@@ -0,0 +1,35 @@
+const { bestScore, calculateScore} = require('./scoreCalculation')
+
+describe('Score Calculation',() => {
+    describe('Checking Inputs',() => {
+      
+  
+      // it('Should return -1 when input is not an array',() => {
+      //     const result = doubleNumbers('abc');
+      //     expect(result).toBe(-1)
+      // })
+  
+      it('Should throw an error when input is not an array',() => {
+              
+             
+        expect(()=> calculateScore('abs')).toThrow('Input is not an array');
+      });
+  
+      it('Should throw an error when array has an element which is not a number',() => {
+              
+             
+        expect(()=> calculateScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 'hello', 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toThrow('Array should consist of numbers only');
+      });
+
+      it('Should throw an error when frame size is not valid',() => {
+              
+             
+        expect(()=> calculateScore([3, 6, 3, 6, 6, 3, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toThrow('Invalid Input');
+      });
+
+
+      
+
+    })
+  
+    });
