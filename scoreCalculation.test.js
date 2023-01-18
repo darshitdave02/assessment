643b0e0027b64d482b13824a647c989330765b32
Darshit Dave <darshitdave02@gmail.com>
2023-01-18 13:52:09 +0530

    chore: add the given example test cases

--- scoreCalculation.test.js
+++ scoreCalculation.test.js
@@ -2,7 +2,22 @@ const {  calculateScore} = require('./scoreCalculation');
 
 describe('Score Calculation',() => {
   describe('Checking Inputs',() => {
-  
+
+    it('Test case - 1 ', () => {
+      let result = calculateScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]);
+      expect(result).toBe(90);
+    });
+
+    it('Test case - 2 ', () => {
+      let result = calculateScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]);
+      expect(result).toBe(30);
+    });
+
+    it('Test case - 3 ', () => {
+      let result = calculateScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+      expect(result).toBe(16);
+    });
+
     it('Should throw an error when input is not an array',() => {
               
              
@@ -20,9 +35,6 @@ describe('Score Calculation',() => {
              
       expect(()=> calculateScore([3, 6, 3, 6, 6, 3, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toThrow('Invalid Input');
     });
-
-
-      
 
   });
   
